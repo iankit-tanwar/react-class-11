e62a7717c292984a9641bfffc976bc3543e3fd3e
iankit-tanwar <tankitofficial01.gmail.com>
2023-02-19 19:40:56 +0530

    sec commit

---
+++ src/Pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <>
+    <div>NotFound</div>
+    <button type="button" class="btn btn-primary">go to home page</button>
+    </>
+    
+  )
+}
